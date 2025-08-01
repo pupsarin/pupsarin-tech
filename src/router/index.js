75818bca24fa841775a208d9262a2a5d372a1790
Serhii <38398027+pupsarin@users.noreply.github.com>
2025-08-01 10:28:05 -0500

    merge uat (#9)

--- src/router/index.js
+++ src/router/index.js
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.NODE_ENV === 'production' ? '/pupsarin-tech/' : '/'),
   routes
 })
 
